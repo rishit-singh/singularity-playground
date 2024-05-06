4b8692a1465bd4c45df3196843346dbf8081298a
rishit-singh <rsrishitsingh@gmail.com>
2024-05-05 23:25:49 -0700

    Added SingularityContext.CreateStorage()

--- singularitycontext/singularitycontext.go
+++ singularitycontext/singularitycontext.go
@@ -1,6 +1,7 @@
 package singularitycontext
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -14,8 +15,22 @@ type APIConfig struct {
 	URL      string
 }
 
+type DatasetMetadata struct {
+	URL             string
+	MD5             string
+	DataCollection  string
+	DataType        string
+	AnalysisGroup   string
+	Sample          string
+	Population      string
+	DataReusePolicy string
+}
+
 type SingularityContext struct {
 	Config APIConfig
+}
+
+type StorageRequest struct {
 }
 
 func (ctx *SingularityContext) GetPreparations() ([]any, error) {
@@ -24,6 +39,42 @@ func (ctx *SingularityContext) GetPreparations() ([]any, error) {
 	authRequest := util.NewBasicAuthRequest(request, util.Credentials{Username: ctx.Config.Username, Password: ctx.Config.Password})
 
 	response, err := http.DefaultClient.Do(authRequest.Request)
+
+	if err != nil {
+		fmt.Print(err)
+		return nil, err
+	}
+
+	body, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		fmt.Printf("Failed to read response body: %s\n", err)
+
+		return nil, err
+	}
+
+	var preparations []any
+
+	err = json.Unmarshal(body, &preparations)
+
+	if err != nil {
+		fmt.Printf("Failed to parse response body: %s\n", err)
+
+		return nil, err
+	}
+
+	return preparations, nil
+}
+
+func (ctx *SingularityContext) CreateStorage(name string, path string, metadata DatasetMetadata) (any, error) {
+	data, _ := util.ToJson(metadata)
+
+	request, err := http.NewRequest("POST", fmt.Sprintf("%s%s", ctx.Config.URL, "preparation"), bytes.NewBuffer([]byte(data)))
+
+	request.Header.Set("Content-Type", "application/json")
+	request.SetBasicAuth(ctx.Config.Username, ctx.Config.Password)
+
+	response, err := http.DefaultClient.Do(request)
 
 	if err != nil {
 		fmt.Print(err)
